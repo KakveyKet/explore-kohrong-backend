803c652aae11ae630ccde0a3a857956923e1e785
KakveyKet <kakveyket408@gmail.com>
2026-09-25 22:51:16 +0700

    Fix blog update route and controller

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -8,13 +8,13 @@ import {
   updateBlog,
 } from "../controllers/blogController.js";
 
-import { authenticate } from "../middleware/auth.js";
+import { authenticate, allowRoles } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /*
 |--------------------------------------------------------------------------
-| BLOG CRUD
+| PUBLIC
 |--------------------------------------------------------------------------
 */
 
@@ -22,10 +22,59 @@ router.get("/", listBlogs);
 
 router.get("/:id", getBlog);
 
-router.post("/", authenticate, createBlog);
+/*
+|--------------------------------------------------------------------------
+| ADMIN
+|--------------------------------------------------------------------------
+*/
 
-router.put("/:id", authenticate, updateBlog);
+router.post("/", authenticate, allowRoles("SUPER_ADMIN", "ADMIN"), createBlog);
 
-router.delete("/:id", authenticate, deleteBlog);
+/*
+|--------------------------------------------------------------------------
+| UPDATE BLOG
+|--------------------------------------------------------------------------
+|
+| Frontend uses:
+|
+| PATCH /api/blogs/:id
+|
+*/
+
+router.patch(
+  "/:id",
+  authenticate,
+  allowRoles("SUPER_ADMIN", "ADMIN"),
+  updateBlog,
+);
+
+/*
+|--------------------------------------------------------------------------
+| OPTIONAL PUT SUPPORT
+|--------------------------------------------------------------------------
+|
+| Keep this if older frontend code used PUT.
+|
+*/
+
+router.put(
+  "/:id",
+  authenticate,
+  allowRoles("SUPER_ADMIN", "ADMIN"),
+  updateBlog,
+);
+
+/*
+|--------------------------------------------------------------------------
+| DELETE
+|--------------------------------------------------------------------------
+*/
+
+router.delete(
+  "/:id",
+  authenticate,
+  allowRoles("SUPER_ADMIN", "ADMIN"),
+  deleteBlog,
+);
 
 export default router;
